01238e146243c8ef27a4fc5800f8b22195b978ee
Michael Thiessen <electromikenetic@gmail.com>
2024-03-25 16:35:53 -0400

    Use Transition component

--- components/content/ProseImg.vue
+++ components/content/ProseImg.vue
@@ -4,30 +4,31 @@
       class="rounded-xl shadow-lg w-full"
       :src="src"
       :alt="alt"
-      @click.stop="toggleLightbox"
+      @click.stop="() => (showLightbox = !showLightbox)"
       width="800"
       sizes="sm:600px md:800px"
       densities="x1 x2"
     />
   </div>
   <Teleport to="body">
-    <div
-      v-if="showLightbox"
-      class="z-10 fixed bottom-0 right-0 top-0 left-0 bg-black bg-opacity-50 flex items-center justify-center backdrop-blur-sm transition-all duration-300 md:p-8"
-      :class="{
-        'opacity-0': !transition,
-        'opacity-100': transition,
-      }"
-      @click.stop="toggleLightbox"
+    <Transition
+      enter-from-class="opacity-0"
+      leave-to-class="opacity-0"
     >
-      <NuxtImg
-        :src="src"
-        :alt="alt"
-        width="6000"
-        sizes="sm:600px md:800px lg:1600px xl:6000px"
-        densities="x1 x2"
-      />
-    </div>
+      <div
+        v-if="showLightbox"
+        class="z-10 fixed bottom-0 right-0 top-0 left-0 bg-black bg-opacity-50 flex items-center justify-center backdrop-blur-sm transition-all duration-300 md:p-8"
+        @click.stop="() => (showLightbox = !showLightbox)"
+      >
+        <NuxtImg
+          :src="src"
+          :alt="alt"
+          width="6000"
+          sizes="sm:600px md:800px lg:1600px xl:6000px"
+          densities="x1 x2"
+        />
+      </div>
+    </Transition>
   </Teleport>
 </template>
 
@@ -44,21 +45,4 @@ defineProps({
 });
 
 const showLightbox = ref(false);
-const transition = ref(false);
-
-async function sleep(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-async function toggleLightbox() {
-  if (showLightbox.value) {
-    transition.value = false;
-    await sleep(300);
-    showLightbox.value = false;
-  } else {
-    showLightbox.value = true;
-    await sleep(300);
-    transition.value = true;
-  }
-}
 </script>
